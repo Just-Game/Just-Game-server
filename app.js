fac379d5de5842330700245ea62cb5687ccaef38
HyeockJinKim <kherootz@gmail.com>
2019-02-15 14:18:16 +0900

    게임정보를 이름으로 받게 수정
    기존에 게임의 id로 게임정보를 받던 것에서
    게임의 이름을 통해 정보를 받도록 수정함
    
    Closes #10

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express = require('express');
 const app = express();
 
 const { allGameNames, getGameInfo } = require('./query');
-const { isNumeric } = require('./utils/number');
 
-app.get('/games/info/:id', async (req, res) => {
-  const num = req.params.id;
-  let result = [];
-  if (isNumeric(num)) {
-    const a = await getGameInfo(parseInt(num))
-    result = a.rows;
-  }
+app.get('/games/info/:slug', async (req, res) => {
+  const num = req.params.slug;
+  const a = await getGameInfo(num)
+  let result = a.rows;
 
   res.status(200).json(result)
 });
